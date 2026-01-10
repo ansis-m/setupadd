c83015fd38c4f04fcd465c43f9c5796caf102244
ansis@home <ansis@home>
2026-01-10 15:31:46 +0200

    add primitive pagination

--- src/views/MarketsView.tsx
+++ src/views/MarketsView.tsx
@@ -13,6 +13,7 @@ import {
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { useMarketsStore } from '../stores/markets.store';
+import { MarketsPagination } from '../components/MarketsPagination';
 
 const useStyles = makeStyles((theme) => ({
   loading: {
@@ -65,42 +66,45 @@ export const MarketsView: React.FC = () => {
   }
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>#</TableCell>
-            <TableCell>Coin</TableCell>
-            <TableCell align="right">Price</TableCell>
-            <TableCell align="right">24h %</TableCell>
-            <TableCell align="right">Market Cap</TableCell>
-            <TableCell align="right">Volume (24h)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {markets.map((coin) => (
-            <TableRow key={coin.id}>
-              <TableCell>{coin.market_cap_rank}</TableCell>
-              <TableCell>
-                <img src={coin.image} alt={coin.name} className={classes.coinImage} />
-                {coin.name}{' '}
-                <Typography variant="caption" color="textSecondary">
-                  ({coin.symbol.toUpperCase()})
-                </Typography>
-              </TableCell>
-              <TableCell align="right">${coin.current_price.toLocaleString()}</TableCell>
-              <TableCell
-                align="right"
-                className={coin.price_change_percentage_24h && coin.price_change_percentage_24h >= 0 ? classes.positive : classes.negative}
-              >
-                {coin.price_change_percentage_24h ? `${coin.price_change_percentage_24h.toFixed(2)}%` : 'N/A'}
-              </TableCell>
-              <TableCell align="right">${coin.market_cap.toLocaleString()}</TableCell>
-              <TableCell align="right">${coin.total_volume.toLocaleString()}</TableCell>
+    <>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>#</TableCell>
+              <TableCell>Coin</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">24h %</TableCell>
+              <TableCell align="right">Market Cap</TableCell>
+              <TableCell align="right">Volume (24h)</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {markets.map((coin) => (
+              <TableRow key={coin.id}>
+                <TableCell>{coin.market_cap_rank}</TableCell>
+                <TableCell>
+                  <img src={coin.image} alt={coin.name} className={classes.coinImage} />
+                  {coin.name}{' '}
+                  <Typography variant="caption" color="textSecondary">
+                    ({coin.symbol.toUpperCase()})
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">${coin.current_price.toLocaleString()}</TableCell>
+                <TableCell
+                  align="right"
+                  className={coin.price_change_percentage_24h && coin.price_change_percentage_24h >= 0 ? classes.positive : classes.negative}
+                >
+                  {coin.price_change_percentage_24h ? `${coin.price_change_percentage_24h.toFixed(2)}%` : 'N/A'}
+                </TableCell>
+                <TableCell align="right">${coin.market_cap.toLocaleString()}</TableCell>
+                <TableCell align="right">${coin.total_volume.toLocaleString()}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <MarketsPagination />
+    </>
   );
 };
